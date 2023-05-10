82d05b91fd9102375d20e6daffcda9779155ce68
Ahmed <ahmadou2808@gmail.com>
2023-05-11 00:47:49 +0100

    fix: remove useless code

--- src/main/java/com/anywr/ahmedtest/web/rest/errors/GlobalExceptionHandler.java
+++ src/main/java/com/anywr/ahmedtest/web/rest/errors/GlobalExceptionHandler.java
@@ -20,8 +20,6 @@ import jakarta.annotation.Nonnull;
 @RestControllerAdvice
 public class GlobalExceptionHandler{
 	
-    private static final String FIELD_ERRORS_KEY = "fieldErrors";
-    private static final String MESSAGE_KEY = "message";
     @ExceptionHandler(LoginAlreadyUsedException.class)
     public ResponseEntity<String> handleException(LoginAlreadyUsedException ex) {
         return ResponseEntity.badRequest().body("login already exists");
@@ -31,31 +29,6 @@ public class GlobalExceptionHandler{
     public ResponseEntity<String> handleException(BadRequestAlertException ex) {
         return ResponseEntity.badRequest().body(ex.getMessage());
     }
-    
-//    @Override
-//    public ResponseEntity<Problem> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, @Nonnull NativeWebRequest request) {
-//        BindingResult result = ex.getBindingResult();
-//        List<FieldErrorVM> fieldErrors = result
-//            .getFieldErrors()
-//            .stream()
-//            .map(f ->
-//                new FieldErrorVM(
-//                    f.getObjectName().replaceFirst("DTO$", ""),
-//                    f.getField(),
-//                    StringUtils.isNotBlank(f.getDefaultMessage()) ? f.getDefaultMessage() : f.getCode()
-//                )
-//            )
-//            .collect(Collectors.toList());
-//
-//        Problem problem = Problem
-//            .builder()
-//            .withTitle("Method argument not valid")
-//            .withStatus(defaultConstraintViolationStatus())
-//            .with(MESSAGE_KEY, "validation error")
-//            .with(FIELD_ERRORS_KEY, fieldErrors)
-//            .build();
-//        return create(ex, problem, request);
-//    }
     
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ValidationResponse> handleValidationException(MethodArgumentNotValidException ex) {
